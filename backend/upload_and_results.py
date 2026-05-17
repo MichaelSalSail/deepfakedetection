11f97dd2074845a0388d0005667936562d72ab2b
MichaelSalSail <michaelsalamon78@gmail.com>
2026-05-16 21:27:55 -0400

    Avoid CORS error on Mac by moving port to 5001.

--- backend/upload_and_results.py
+++ backend/upload_and_results.py
@@ -1,25 +1,30 @@
 from flask import Flask, render_template, redirect, url_for, request
 from flask_restful import Api, Resource, reqparse
-from flask_cors import CORS, cross_origin
 import os
 import json
 import time
 APP_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_PATH = os.path.join(APP_PATH, 'src/pages/')
 app = Flask(__name__, template_folder=TEMPLATE_PATH)
-cors = CORS(app)
 
-# This file aims to download the user uploaded video to the 
-# appropriate local directory and update the user on any 
+@app.after_request
+def add_cors_headers(response):
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Methods'] = 'GET, POST, OPTIONS'
+    response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
+    return response
+
+# This file aims to download the user uploaded video to the
+# appropriate local directory and update the user on any
 # changes to result_update.json
 
-@app.route('/home/results', methods = ['GET'])
+@app.route('/home/results', methods = ['GET', 'OPTIONS'])
 def success():
     # Since we are looking for a file modification in the last 2 seconds,
     # delaying for 2 seconds ensures that we won't return results from a
     # previous 'Generate Results' run.
     time.sleep(2)
-    file_dir=os.path.join(APP_PATH,'src/utils/AllResultsJSON/result_update.json')
+    file_dir=os.path.join(APP_PATH,'backend/AllResults/result_update.json')
     curr_time=time.time()
     file_mod_time=os.path.getmtime(file_dir)
     while(abs(curr_time-file_mod_time)>2):
@@ -31,12 +36,11 @@ def success():
     all_data[3]["shades"]=int(all_data[3]["shades"])
     return {"models" : all_data}
 
-@app.route('/home/upload', methods = ['POST'])
-@cross_origin()
+@app.route('/home/upload', methods = ['POST', 'OPTIONS'])
 def upload_video():
     if request.method == 'POST':
         (request.files["file"]).save(os.path.join(APP_PATH,'backend/example_videos/target.mp4'))
         return "Successfully saved!"
 
 if __name__ == '__main__':
-   app.run(port=5000, debug = True)# 3000 , 5000
+   app.run(port=5001, debug = True)
