e4190d1f1466d7b0902d6736706ca01da8e207a2
MichaelSalSail <michaelsalamon78@gmail.com>
2026-06-14 15:39:31 -0400

    Added terminal output for models total runtime.

--- backend/run_backend.py
+++ backend/run_backend.py
@@ -1,6 +1,8 @@
 import os, sys
+import time
+from datetime import datetime
 import torch
-from all_models import predict_on_video, blink_on_video, detect_age_gender, detect_shades
+from all_models import predict_on_video, blink_on_video, detect_age_gender, detect_shades, _format_runtime
 from helper_functions import get_model, write_result_update_json
 
 # -----------------------------------------Look here-----------------------------------------
@@ -37,6 +39,9 @@ model_for_tests=get_model()
 
 print("\nIgnore any warnings above. So far, so good.")
 
+start_perf = time.perf_counter()
+print("start time: " + datetime.now().strftime("%H:%M:%S"))
+
 # Run all models
 dfd_result = predict_on_video(video_path, 15, device, facedet)
 blink_result = blink_on_video(video_path, 15, facedet, model_for_tests)
@@ -47,10 +52,14 @@ age_gender_result = detect_age_gender(
 )
 shades_result = detect_shades(temp_img_subject_reference, temp_img_tight_crop)
 
+print("end time: " + datetime.now().strftime("%H:%M:%S"))
+
 write_result_update_json(
     [dfd_result, blink_result, age_gender_result, shades_result],
     results_path,
 )
+
+print("total runtime: " + _format_runtime(time.perf_counter() - start_perf))
 
 # TensorFlow and PyTorch cleanup routines conflict on shutdown and cause a
 # segfault. All results are written to disk before this point, so bypassing
